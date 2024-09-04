2934cb0e4acb8a28a31f2e51c1516994f3d45c18
Morgan <morno368@student.liu.se>
2024-09-04 09:47:32 +0200

    hopes and dreams

--- lab2/interpreter.pl
+++ lab2/interpreter.pl
@@ -12,3 +12,32 @@
 
 % state might be a list [a = N, b = N, ..., C = N] ?
 % and program should be defined as an IMP struct ?
+
+
+evalBoolExpr(tt, tt).
+evalBoolExpr(ff, ff).
+evalBoolExpr(E, ==, E).
+evalBoolExpr(E, <, E).
+evalBoolExpr(E, <=, E).
+evalBoolExpr(E, >, E).
+evalBoolExpr(E, >=, E).
+
+evalExpr(id, Val, Val).
+evalExpr(num, N, N).
+evalExpr(A, +, B, Res) :- plus(A, B, Res).
+evalExpr(A, -, B, Res) :- plus(A, B, Res).
+evalExpr(A, /, B, Res) :- plus(A, B, Res).
+evalExpr(A, *, B, Res) :- plus(A, B, Res).
+
+
+execCommand(skip).
+
+execCommand(set, I, E).
+
+execCommand(if, B, C, C).
+
+execCommand(while, B, C).
+
+execCommand(seq, C, C).
+
+execute(InitialState, Program, FinalState).
