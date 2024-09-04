426458e562298cf46252add76224b38c94925994
Morgan <morno368@student.liu.se>
2024-09-04 16:23:05 +0200

    Hopes and dreams were crushed. reduced to ashes... but from the ashes rises a pheonix of new hope. Even grander than before. But this time might actually work. Not tested, lots of new code

--- lab2/interpreter.pl
+++ lab2/interpreter.pl
@@ -5,49 +5,99 @@
 % E = id(I) | Num(N) | E + E | ...
 % C = skip | set(I, E) | if(B, C, C) | while(B, C) | seq(C, C)
 
-% TODO:
-% Represent I and N
-% Define arithmetic and boolean expressions
-% Lastly, execute/3 which takes state and program and returns resulting state
+num(N) :-
+  integer(N),
+  N >= 0.
 
-% state might be a list [a = N, b = N, ..., C = N] ?
-% and program should be defined as an IMP struct ?
-
+id(I) :-
+  string(I).
 
 evalBoolExpr(tt, tt).
 evalBoolExpr(ff, ff).
-evalBoolExpr(E, ==, E).
-evalBoolExpr(E, <, E).
-evalBoolExpr(E, <=, E).
-evalBoolExpr(E, >, E).
-evalBoolExpr(E, >=, E).
 
-evalExpr(id, Val, Val).
-evalExpr(num, N, N).
-evalExpr(A, +, B, Res) :- Res is A + B.
-evalExpr(A, -, B, Res) :- Res is A - B.
-evalExpr(A, /, B, Res) :- Res is A / B.
-evalExpr(A, *, B, Res) :- Res is A * B.
+evalBoolExpr(State, A==B, tt) :-
+  evalExpr(State, A, LeftRes),
+  evalExpr(State, B, RightRes),
+  LeftRes == RightRes.
 
+evalBoolExpr(State, A==B, ff) :-
+  evalExpr(State, A, LeftRes),
+  evalExpr(State, B, RightRes),
+  LeftRes == RightRes.
 
-execCommand([skip], _).
+evalBoolExpr(State, A<B, tt) :-
+  evalExpr(State, A, LeftRes),
+  evalExpr(State, B, RightRes),
+  LeftRes < RightRes.
 
-execCommand([set, I, E], State).
+evalBoolExpr(State, A<B, ff) :-
+  evalExpr(State, A, LeftRes),
+  evalExpr(State, B, RightRes),
+  LeftRes >= RightRes.
 
-execCommand([if, B, TrueBranch, FalseBranch], _).
-  evalBoolExpr(B, Res),
+evalBoolExpr(State, A>B, tt) :-
+  evalExpr(State, A, LeftRes),
+  evalExpr(State, B, RightRes),
+  RightRes < LeftRes.
+
+evalBoolExpr(State, A>B, ff) :-
+  evalExpr(State, A, LeftRes),
+  evalExpr(State, B, RightRes),
+  RightRes >= LeftRes.
+
+evalExpr(State, id(I), Value) :- member([I, Value], State).
+evalExpr(State, N, N) :- num(N).
+
+evalExpr(State, A+B, Res) :- 
+  evalExpr(State, A, Avalue),
+  evalExpr(State, B, Bvalue),
+  Res is Avalue + Bvalue.
+
+evalExpr(State, A-B, Res) :- 
+  evalExpr(State, A, Avalue),
+  evalExpr(State, B, Bvalue),
+  Res is Avalue - Bvalue.
+
+evalExpr(State, A/B, Res) :- 
+  evalExpr(State, A, Avalue),
+  evalExpr(State, B, Bvalue),
+  Res is Avalue / Bvalue.
+
+evalExpr(State, A*B, Res) :- 
+  evalExpr(State, A, Avalue),
+  evalExpr(State, B, Bvalue),
+  Res is Avalue * Bvalue.
+
+execute(State, skip, State).
+execute(State, set(I, E), NewState)).
+  %TODO
+  %Check if I is already set
+  %Remove old I-value pair
+  %Insert new I-Value pair
+  %And if I-Value pair does not exist, simply add it
+  .
+
+execute(State, if(Condition, TrueBranch, FalseBranch), NewState) :-
+  evalBoolExpr(State, Condition, Res),
   Res == tt,
-  execCommand(TrueBranch).
-  
-execCommand([if, B, TrueBranch, FalseBranch], _).
-  evalBoolExpr(B, Res),
+  execute(State, TrueBranch, NewState).
+
+execute(State, if(Condition, TrueBranch, FalseBranch), NewState) :-
+  evalBoolExpr(State, Condition, Res),
   Res == ff,
-  execCommand(FalseBranch).
+  execute(State, FalseBranch, NewState).
 
-execCommand([while, B, C], State).
+execute(State, while(Condition, Statement), NewState) :-
+  evalBoolExpr(State, Condition, Res),
+  Res == tt,
+  execute(State, Statement, InbetweenState),
+  execute(InbetweenState, while(Condition, Statement), NewState).
 
-execCommand([seq, C, C1], State).
-  execCommand(C),
-  execCommand(C1).
+execute(State, while(Condition, Statement), State) :-
+  evalBoolExpr(State, Condition, Res),
+  Res == ff.
 
-execute(InitialState, Program, FinalState).
+execute(State, seq(C, C1), NewState) :-
+  execute(State, C, InbetweenState),
+  execute(InbetweenState, C1, NewState).
+
