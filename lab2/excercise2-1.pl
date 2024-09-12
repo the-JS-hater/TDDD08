9dc3e59b28afa69497e6dfb937083a3e2421104c
Morgan <morno368@student.liu.se>
2024-09-12 15:21:41 +0200

    restructured lab 2

--- lab2/excercise2-1.pl
+++ lab2/excercise2-1.pl
@@ -19,17 +19,21 @@ ssort(L, L1) :-
 
 
 % collect smallest element of a list
+% base case
 smallest([X], X).
 
+% First element is smaller than second, so second is discarded and we proceed recursivley
 smallest([H, H1 | T], S) :-
   H1 >= H,
   smallest([H | T], S).
 
+% Second element is smaller than head, so head is discarded and we proceed recursivley
 smallest([H, H1 | T], S) :-
   H >= H1,
   smallest([H1 | T], S).
 
 % remove from empty list
+% base case
 remove(X, [], []).
 
 % remove X when X is the head of the list
@@ -44,6 +48,10 @@ remove(X, [H | T], [H | T1]) :-
 qsort([], []).
 qsort([X], [X]).
 
+% main function for qsort containing entirety of the algorithm
+% partition based on head elem into lists containg smaller and larger
+% values respectivley, and recursivley sort each, then append
+% into smaller partition -> head elem -> larger partition
 qsort([H | T], L1) :-
   partition(T, H, Small, Large),
   qsort(Small, Small2),
@@ -51,16 +59,25 @@ qsort([H | T], L1) :-
   append(Small2, [H], L2),
   append(L2, Large2, L1).
 
+% base case for the partitioning
 partition([], N, [], []).
 
+% When first elem is smaller than the partitioning value
 partition([H | T], N, Small, Large) :-
   H < N,
   partition(T, N, Small2, Large),
   append([H], Small2, Small).
 
+% When first elem is larger than(or equal to) the partitioning value
 partition([H | T], N, Small, Large) :-
   H >= N,
   partition(T, N, Small, Large1),
   append([H], Large1, Large).
 
-%:- initialization forall(qsort([-7, 3, -2, 5, 5, 7, -1, 10], T), writeln(T)).
+% Test queries that are executed when the script is included in the interpreter
+% Usage: swipl <path-to-this-file.pl>
+:- initialization writeln("original list: -7, 3, -2, 5, 5, 7, -1, 10").
+:- initialization writeln("Selection sort: ").
+:- initialization forall(ssort([-7, 3, -2, 5, 5, 7, -1, 10], T), writeln(T)).
+:- initialization writeln("Quick sort: ").
+:- initialization forall(qsort([-7, 3, -2, 5, 5, 7, -1, 10], T), writeln(T)).
--- lab2/excercise2-3.pl
+++ lab2/excercise2-3.pl
@@ -10,122 +10,164 @@ num(N) :-
   integer(N),
   N >= 0.
 
-
+% Variable names can be any prolog atom
 id(I) :-
   atom(I).
 
+% evaluating true and false
+% serves as base cases for more complex boolean expressions
 evalBoolExpr(tt, tt).
 evalBoolExpr(ff, ff).
 
+% evaluate == when it results in true(tt)
 evalBoolExpr(State, A==B, tt) :-
   evalExpr(State, A, LeftRes),
   evalExpr(State, B, RightRes),
   LeftRes == RightRes.
 
+% evaluate == when it results in false(ff)
 evalBoolExpr(State, A==B, ff) :-
   evalExpr(State, A, LeftRes),
   evalExpr(State, B, RightRes),
   LeftRes \= RightRes.
 
+% evaluate < when it results in true(tt)
 evalBoolExpr(State, A<B, tt) :-
   evalExpr(State, A, LeftRes),
   evalExpr(State, B, RightRes),
   LeftRes < RightRes.
 
+% evaluate < when it results in false(ff)
 evalBoolExpr(State, A<B, ff) :-
   evalExpr(State, A, LeftRes),
   evalExpr(State, B, RightRes),
   LeftRes >= RightRes.
 
+% evaluate > when it results in true(tt)
 evalBoolExpr(State, A>B, tt) :-
   evalExpr(State, A, LeftRes),
   evalExpr(State, B, RightRes),
   RightRes < LeftRes.
 
+% evaluate > when it results in false(ff)
 evalBoolExpr(State, A>B, ff) :-
   evalExpr(State, A, LeftRes),
   evalExpr(State, B, RightRes),
   RightRes >= LeftRes.
 
+% evaluating a variable by extracting it's value from the State
 evalExpr(State, id(I), Value) :- member(I=Value, State).
+
+% evaluating a constant
 evalExpr(State, num(N), N) :- num(N).
 
+% evaluate addition
 evalExpr(State, A+B, Res) :- 
   evalExpr(State, A, Avalue),
   evalExpr(State, B, Bvalue),
   Res is Avalue + Bvalue.
 
+% evaluate subtraction
 evalExpr(State, A-B, Res) :- 
   evalExpr(State, A, Avalue),
   evalExpr(State, B, Bvalue),
   Res is Avalue - Bvalue.
 
+% evaluate division
 evalExpr(State, A/B, Res) :- 
   evalExpr(State, A, Avalue),
   evalExpr(State, B, Bvalue),
   Res is Avalue / Bvalue.
 
+% evaluate multiplication
 evalExpr(State, A*B, Res) :- 
   evalExpr(State, A, Avalue),
   evalExpr(State, B, Bvalue),
   Res is Avalue * Bvalue.
 
-
+% base case for variable name not being defined
 notExists([], I).
+
+% when the variable name is not the head
 notExists([H=_ | T], I) :-
   H \= I,
   notExists(T, I).
 
+% Variable is already defined
 exists(State, I) :-
   member(I=_, State).
 
+% Replacing the assigned value of a defined variable
+% Recursive case when the identifier is not the head
 replace([H=SomeValue | T], I, E, NewState) :-
   H \= I,
   replace(T, I, E, Modified),
   append([H=SomeValue], Modified, NewState).
 
+% Base case when the identifier is the head
 replace([H=_| T], I, E, [H=E | T]) :-
   H == I.
 
+% execute Skip instruction. No change in state occurs
 execute(State, skip, State).
 
+% execute assignment on a previously undefined variable
+% evaluate expression and append I=<the results> to State
 execute(State, set(I, E), NewState) :-
   notExists(State, I),
   evalExpr(State, E, ExprEvaluation),
   append(State, [I=ExprEvaluation], NewState).
 
+% Execute assignment on a defined variable
+% Replace it's value
 execute(State, set(I, E), NewState) :-
   exists(State, I),
   evalExpr(State, E, ExprEvaluation),
   replace(State, I, ExprEvaluation, NewState).
 
+% execute If-statment
+% This is when the True Branch should execute
 execute(State, if(Condition, TrueBranch, FalseBranch), NewState) :-
   evalBoolExpr(State, Condition, Res),
   Res == tt,
   execute(State, TrueBranch, NewState).
 
+% This is when the False branch should execute
 execute(State, if(Condition, TrueBranch, FalseBranch), NewState) :-
   evalBoolExpr(State, Condition, Res),
   Res == ff,
   execute(State, FalseBranch, NewState).
 
+% Execute while-statement
+% This is when the condition holds and the loop
+% should continue. Uses inbetween states to update
+% state between iterations
 execute(State, while(Condition, Statement), NewState) :-
   evalBoolExpr(State, Condition, Res),
   Res == tt,
   execute(State, Statement, InbetweenState),
   execute(InbetweenState, while(Condition, Statement), NewState).
 
+% Base case for the while loop where it ends since the condition no longer
+% holds true
 execute(State, while(Condition, Statement), State) :-
   evalBoolExpr(State, Condition, Res),
   Res == ff.
 
+% execute a sequence of two statements. InbetweenState
+% is needed to keep track of the three stage transition
+% of the programs state, since a change in state from C
+% might affect the execution of C1
 execute(State, seq(C, C1), NewState) :-
   execute(State, C, InbetweenState),
   execute(InbetweenState, C1, NewState).
 
+% Test quearies with several programs, including the example shown in the lab PM
+% These are executed when included in the interpreter
 :- forall(execute([x=2], seq(set(y, id(x)+num(5)), set(z, id(x)*id(y))), Sn), writeln(Sn)).
 :- forall(evalBoolExpr([x=1], id(x)<num(10), Res), writeln(Res)).
 :- forall(execute([x=10], set(x, id(x)+num(1)), Sn), writeln(Sn)).
 :- forall(execute([x=1], while(id(x)<num(10), set(x, id(x)+num(1))), Sn), writeln(Sn)).
 :- forall(execute([x=3, y=1], seq(set(y, id(x)*id(y)), set(x, id(x)-num(1))), Sn), writeln(Sn)).
+% This is the testcase from the lab pm. It should (and does) result in the state [x=1, y=6]
 :- forall(execute([x=3], seq(set(y, num(1)), while(id(x)>num(1), seq(set(y, id(y)*id(x)), set(x, id(x)-num(1))))), Sn), writeln(Sn)).
